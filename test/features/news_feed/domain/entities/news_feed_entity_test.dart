afe80ceaa30375e46f0c1731c4ebbc77c57ee901
BasakK6 <katukbasak@gmail.com>
2023-09-29 18:52:56 +0300

    test: refactored the unnecessary usage of NewsFeedEntity class when testing ArticlesEntity class

--- test/features/news_feed/domain/entities/news_feed_entity_test.dart
+++ test/features/news_feed/domain/entities/news_feed_entity_test.dart
@@ -5,83 +5,63 @@ void main() {
   group("timeAgo tests", () {
     test("should return null when publishedAt is null", () {
       //arrange
-      const testNewsFeedEntity = NewsFeedEntity(
-        articles: [
-          ArticlesEntity(
-              //publishedAt: "2023-09-24T05:35:00Z",
-              )
-        ],
+      const testArticlesEntity = ArticlesEntity(
+        //publishedAt: "2023-09-24T05:35:00Z",
       );
       //act
-      final result = testNewsFeedEntity.articles?[0].timeAgo;
+      final result = testArticlesEntity.timeAgo;
       //assert
       expect(result, "Unknown");
     });
 
     test("should return Just now for very recent time", () {
       //arrange
-      final testNewsFeedEntity = NewsFeedEntity(
-        articles: [
-          ArticlesEntity(
-            publishedAt: DateTime.now()
-                .subtract(const Duration(seconds: 5))
-                .toIso8601String(),
-          )
-        ],
+      final testArticlesEntity = ArticlesEntity(
+        publishedAt: DateTime.now()
+            .subtract(const Duration(seconds: 5))
+            .toIso8601String(),
       );
       //act
-      final result = testNewsFeedEntity.articles?[0].timeAgo;
+      final result = testArticlesEntity.timeAgo;
       //assert
       expect(result, equals('Just now'));
     });
 
     test("should return time difference in minutes", () {
       //arrange
-      final testNewsFeedEntity = NewsFeedEntity(
-        articles: [
-          ArticlesEntity(
-            publishedAt: DateTime.now()
-                .subtract(const Duration(minutes: 30))
-                .toIso8601String(),
-          )
-        ],
+      final testArticlesEntity = ArticlesEntity(
+        publishedAt: DateTime.now()
+            .subtract(const Duration(minutes: 30))
+            .toIso8601String(),
       );
       //act
-      final result = testNewsFeedEntity.articles?[0].timeAgo;
+      final result = testArticlesEntity.timeAgo;
       //assert
       expect(result, equals('30 Minutes ago'));
     });
 
     test("returns time difference in hours", () {
       //arrange
-      final testNewsFeedEntity = NewsFeedEntity(
-        articles: [
-          ArticlesEntity(
-            publishedAt: DateTime.now()
-                .subtract(const Duration(hours: 3))
-                .toIso8601String(),
-          )
-        ],
+      final testArticlesEntity = ArticlesEntity(
+        publishedAt: DateTime.now()
+            .subtract(const Duration(hours: 3))
+            .toIso8601String(),
       );
       //act
-      final result = testNewsFeedEntity.articles?[0].timeAgo;
+      final result = testArticlesEntity.timeAgo;
       //assert
       expect(result, equals('3 Hours ago'));
     });
 
     test("should return time difference in days", () {
       //arrange
-      final testNewsFeedEntity = NewsFeedEntity(
-        articles: [
-          ArticlesEntity(
-            publishedAt: DateTime.now()
-                .subtract(const Duration(days: 3))
-                .toIso8601String(),
-          )
-        ],
+      final testArticlesEntity = ArticlesEntity(
+        publishedAt: DateTime.now()
+            .subtract(const Duration(days: 3))
+            .toIso8601String(),
       );
       //act
-      final result = testNewsFeedEntity.articles?[0].timeAgo;
+      final result = testArticlesEntity.timeAgo;
       //assert
       expect(result, equals('3 Days ago'));
     });
